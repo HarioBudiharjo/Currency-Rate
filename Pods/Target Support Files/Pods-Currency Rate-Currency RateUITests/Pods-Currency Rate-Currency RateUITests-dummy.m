528911f20c342505543d6d3dd880725a6bc2f4af
Hario Budiharjo <hariobudiharjo@Gmail.com>
2020-02-02 02:32:21 +0700

    File from pod

---
+++ Pods/Target Support Files/Pods-Currency Rate-Currency RateUITests/Pods-Currency Rate-Currency RateUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Currency_Rate_Currency_RateUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Currency_Rate_Currency_RateUITests
+@end
