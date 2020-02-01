d0f8023340d42f3e7db1231210418f52f8d52501
Hario Budiharjo <hariobudiharjo@Gmail.com>
2020-02-01 21:50:35 +0700

    Initial Commit

---
+++ Currency Rate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Currency Rate
+//
+//  Created by Hario Budiharjo on 01/02/20.
+//  Copyright © 2020 Hario Budiharjo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
